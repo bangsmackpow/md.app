1f03b09f2a1d726e743d556298a4eaef39ef4bde
bangsmackpow <username@users.noreply.github.com>
2026-03-16 11:22:42 -0500

    "first push with bun"

---
+++ android/app/src/main/java/com/builtnetworks/app/mk/MainActivity.java
@@ -0,0 +1,5 @@
+package com.builtnetworks.app.mk;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Forces Next.js to generate static HTML/CSS/JS
+  images: {
+    unoptimized: true, // Required because the mobile app can't optimize images on the fly
+  }
 };
 
 export default nextConfig;
